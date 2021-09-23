3771b87434dc85300f0b06f831f7332dce80ebae
Neemo1 <90902672+Neemo1@users.noreply.github.com>
2021-09-23 20:01:33 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,25 +15,21 @@ namespace LabRob2
         }
         static void Task1()
         {
-
+            Console.WriteLine("Завдання 1");
             OperatingSystem os = System.Environment.OSVersion;
             Console.WriteLine("Platform: {0}", os.Platform);
             System.Console.WriteLine("The current date and time so " + System.DateTime.Now);
-            
-
+            System.Console.ReadLine();
         }
         static double Task2()
         {
-            System.Console.ReadLine();
-            // x2, y;
-            Console.WriteLine("Input x = \r");
+            Console.WriteLine("Завдання 2");
+            System.Console.WriteLine("The current date and time is = " + System.DateTime.Now);
+            double pi = 3.14159;
             double x1 = Convert.ToDouble(Console.ReadLine());
-            
-            /*x2 = Convert.ToDouble(Console.ReadLine());
-            y = Convert.ToDouble(Console.ReadLine());*/
-            double K = 1.3 * Math.Exp(-x1 / 2) + Math.Abs(Math.Cos((2 * Math.PI * x1 / 3) - 1.4)) + 6 / 11;
-            Console.WriteLine("x = {0} \t K = {1}", x1, K);
-
+            double K = 1.3 * Math.Exp(-x1 / 2) + Math.Abs(Math.Cos((2 * pi * x1 / 3) - 1.4)) + 6 / 11;
+            Console.WriteLine("x = {0} \t y = {1}", x1, K);
+            System.Console.ReadLine();
             return K;
 
         }
